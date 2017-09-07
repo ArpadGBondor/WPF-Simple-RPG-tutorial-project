e3392491317ea73159dc06c8412431db3259bade
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2017-09-07 22:03:02 +0100

    20.1

--- SuperAdventure/Engine/HealingPotion.cs
+++ SuperAdventure/Engine/HealingPotion.cs
@@ -9,7 +9,8 @@ namespace Engine
     public class HealingPotion : Item
     {
         public int AmountToHeal { get; set; }
-        public HealingPotion(int id, string name, string namePlural, int amountToHeal) : base(id, name, namePlural)
+        public HealingPotion(int id, string name, string namePlural, int amountToHeal, int price)
+            : base(id, name, namePlural, price)
         {
             AmountToHeal = amountToHeal;
         }
